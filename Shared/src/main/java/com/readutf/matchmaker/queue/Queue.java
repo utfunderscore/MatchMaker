815f9f21c8899c6056e7f00744e7e3cc1ad21e31
Connor <concon477@virginmedia.com>
2024-02-18 15:09:34 +0000

    fix(Shared) : Add to queue and queue size methods

--- Shared/src/main/java/com/readutf/matchmaker/queue/Queue.java
+++ Shared/src/main/java/com/readutf/matchmaker/queue/Queue.java
@@ -2,16 +2,14 @@ package com.readutf.matchmaker.queue;
 
 import lombok.Getter;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Predicate;
 
 @Getter
 public class Queue {
 
     private final String name, matchMakerId, serverFilterId;
-    private final Collection<QueueEntry> inQueue;
+    private final List<QueueEntry> inQueue;
     private final int maxTeamSize;
     private final int minTeamSize;
     private final int numberOfTeams;
@@ -20,9 +18,19 @@ public class Queue {
         this.name = name;
         this.matchMakerId = matchMakerId;
         this.serverFilterId = serverFilterId;
-        this.inQueue = new ArrayDeque<>();
+        this.inQueue = new ArrayList<>();
         this.maxTeamSize = maxTeamSize;
         this.minTeamSize = minTeamSize;
         this.numberOfTeams = numberOfTeams;
     }
+
+    public void addToQueue(UUID playerId) {
+        if(inQueue.stream().anyMatch(queueEntry -> queueEntry.getPlayers().contains(playerId))) throw new IllegalArgumentException("Player already in queue");
+        inQueue.add(QueueEntry.create(playerId));
+    }
+
+    public int getQueueSize() {
+        return inQueue.stream().mapToInt(queueEntry -> queueEntry.getPlayers().size()).sum();
+    }
+
 }
